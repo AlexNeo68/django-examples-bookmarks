63b0ec09b6d0e11107669046137cf773e481bba4
Alexander <alexander@iMac-Alexander.local>
2024-02-11 13:08:29 +0300

    views profile user edit forms

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -3,8 +3,8 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
-from .forms import LoginForm, UserRegistrationForm
-
+from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
+from .models import Profile
 
 
 # Create your views here.
@@ -40,6 +40,7 @@ def register(request: HttpRequest):
             new_user = user_form.save(commit=False)
             new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
+            Profile.objects.create(user=new_user)
             return render(request, 'account/register_done.html', {'new_user': new_user})
 
     else:
