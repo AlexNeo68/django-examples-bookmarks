b577cf63d6906cfb57f188d0400d587d861e1e8b
Alexander <alexander@iMac-Alexander.local>
2024-02-13 05:31:28 +0300

    image saves

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -5,10 +5,10 @@ from .views import dashboard, edit, register, user_login
 
 
 urlpatterns = [
-    path('login/', user_login, name='login'),
+    # path('login/', user_login, name='login'),
     # path('login/', auth_views.LoginView.as_view() , name='login'),
     # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', include('django.contrib.auth.urls')),
     path('', dashboard, name='dashboard'),
 
